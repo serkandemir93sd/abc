c0c43738a04019ec0ed7532e656f6b199d19f349
Serkan Demir <serkan.demir93.sd@gmail.com>
2026-09-23 21:34:19 +0200

    Initial sharing of project

---
+++ Laptop.java
@@ -0,0 +1,45 @@
+public class Laptop
+{
+    private String  hersteller;
+    private int     kosten;
+    private boolean neu;
+    
+        public Laptop(String neuHersteller, int neuKosten, boolean neuNeu)
+    {
+        setHersteller(neuHersteller);
+        setKosten(neuKosten);
+        setNeu(neuNeu);
+    }
+    
+
+ public String getHersteller()
+    {
+        return hersteller;
+    }
+
+    public int getKosten()
+    {
+        return kosten;
+    }
+
+    public boolean getNeu()
+    {
+        return neu;
+    }
+    
+    public void setHersteller(String neuHersteller)
+    {
+        hersteller = neuHersteller;
+    }
+
+    public void setKosten(int neuKosten)
+    {
+        kosten = neuKosten;
+    }
+
+    public void setNeu(boolean neuNeu)
+    {
+        neu = neuNeu;
+    }
+}
+
